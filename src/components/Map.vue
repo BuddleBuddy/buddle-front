07d21f8fa377cb8232d019df0c352c690448caac
KeonhoPark <gse04063@gmail.com>
2022-09-21 04:21:00 +0900

    feat: 카카오맵 초기 위치 지정

---
+++ src/components/Map.vue
@@ -0,0 +1,104 @@
+<template>
+  <div>
+    <div id="map"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "KakaoMap",
+  data(){
+    return{
+      defaultMarker:[
+          [33.49200194793989, 126.84315016560106],
+      ]
+    };
+  },
+
+  mounted() {
+    if (window.kakao && window.kakao.maps) {
+      this.initMap();
+    } else {
+      const script = document.createElement("script");
+      /* global kakao */
+      script.onload = () => kakao.maps.load(this.initMap);
+      script.src = "//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=f3bfb3979041ef4a01bff5302e4db826";
+      document.head.appendChild(script);
+    }
+  },
+  methods: {
+    initMap() {
+      const container = document.getElementById("map");
+      const options = {
+        center: new kakao.maps.LatLng(37.49200194793989, 126.84315016560106),
+        level: 4,
+      };
+
+      //지도 객체를 등록합니다.
+      //지도 객체는 반응형 관리 대상이 아니므로 initMap에서 선언합니다.
+      this.map = new kakao.maps.Map(container, options);
+    },
+    displayMarker(markerPositions) {
+      if (this.markers.length > 0) {
+        this.markers.forEach((marker) => marker.setMap(null));
+      }
+
+      const positions = markerPositions.map(
+          (position) => new kakao.maps.LatLng(...position)
+      );
+
+      if (positions.length > 0) {
+        this.markers = positions.map(
+            (position) =>
+                new kakao.maps.Marker({
+                  map: this.map,
+                  position,
+                })
+        );
+
+        const bounds = positions.reduce(
+            (bounds, latlng) => bounds.extend(latlng),
+            new kakao.maps.LatLngBounds()
+        );
+
+        this.map.setBounds(bounds);
+      }
+    },
+    displayInfoWindow() {
+      if (this.infowindow && this.infowindow.getMap()) {
+        //이미 생성한 인포윈도우가 있기 때문에 지도 중심좌표를 인포윈도우 좌표로 이동시킨다.
+        this.map.setCenter(this.infowindow.getPosition());
+        return;
+      }
+
+      var iwContent = '<div style="padding:5px;">Hello World!</div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+          iwPosition = new kakao.maps.LatLng(33.450701, 126.570667), //인포윈도우 표시 위치입니다
+          iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
+
+      this.infowindow = new kakao.maps.InfoWindow({
+        map: this.map, // 인포윈도우가 표시될 지도
+        position: iwPosition,
+        content: iwContent,
+        removable: iwRemoveable,
+      });
+
+      this.map.setCenter(iwPosition);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+#map {
+  width: 400px;
+  height: 300px;
+  margin: auto;
+  overflow: auto;
+  background-color: white;
+  border-radius: 20px;
+
+}
+
+</style>
---
+++ src/views/Introduce.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="root">
+    <img class="intro" alt="intro" src="../assets/intro.png">
+  </div>
+  <div>
+  <KakaoMap/>
+  </div>
+</template>
+
+<script>
+import KakaoMap from "../components/Map";
+
+export default {
+  name: "Intro",
+  components: {
+    KakaoMap,
+  },
+};
+</script>
+
+<style scoped>
+.root {
+  max-width: 500px;
+  min-height: 60vh;
+  height: 100%;
+  overflow: auto;
+  background-color: #E5E5E5;
+}
+
+.root .intro{
+  width: 100%;
+}
+</style>
