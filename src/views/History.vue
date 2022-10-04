5ef820a2bc423d3c63cb31a971483bc2fc81d624
DESKTOP-AVA9CAM\wonee <woneee99@gmail.com>
2022-10-04 18:26:59 +0900

    design: history 공사중

---
+++ src/views/History.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="txt"> 공사중 ! </div>
+</template>
+
+<script>
+export default {
+  name: "History"
+};
+</script>
+
+<style scoped>
+.txt{
+    margin-top: 50%;
+    color: #50A65D;
+    font-weight: 400;
+    font-size: 36px;
+    line-height: 44px;
+    text-align: center;
+}
+</style>
