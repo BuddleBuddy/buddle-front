73cafffd09c1407d20d106b8ff8cdb2d021c4fcb
DESKTOP-AVA9CAM\wonee <woneee99@gmail.com>
2022-10-05 14:57:02 +0900

    fix: img error

--- src/views/Event6.vue
+++ src/views/Event6.vue
@@ -1,36 +1,50 @@
 <template>
-    <div class="container" @click="click()">
-      <img class="icon" align = "left" src="../assets/backIcon.png" @click.stop="back()"> 
-    </div>
+  <div class="container" @click="click()">
+    <img
+      class="icon"
+      align="left"
+      src="../assets/backIcon.png"
+      @click.stop="back()"
+    />
+    <img class="bottom" src="../assets/touch.png" />
+  </div>
 </template>
 
 <script>
 export default {
   name: "Event6",
   methods: {
-    click(){
-      this.$router.push('Event7');
+    click() {
+      this.$router.push("Event7");
     },
-    back(){
-    this.$router.go(-1);
-    }
-  }
+    back() {
+      this.$router.go(-1);
+    },
+  },
 };
 </script>
 
 <style scoped>
-.container{
-    max-width: 500px;
-    min-height: 100vh;
-    background-image: url("../assets/ev6.png");
-    background-repeat: no-repeat;
-    background-size: cover;
-    background-position: center;
+.container {
+  max-width: 500px;
+  min-height: 100vh;
+  background-image: url("../assets/ev6.png");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  position: relative;
 }
 
-.container .icon{
+.container .icon {
   height: 35px;
   width: 35px;
 }
 
+.container .bottom {
+  position: absolute;
+  width: 85%;
+  bottom: 20px;
+  left: 50%;
+  transform: translate(-50%, 0);
+}
 </style>
