8d8d3f7b9cabd44af333418a94bde1ef34b88bcb
KeonhoPark <gse04063@gmail.com>
2022-10-05 19:12:47 +0900

    feat: event6 에니메이션

--- src/views/Event6.vue
+++ src/views/Event6.vue
@@ -1,21 +1,62 @@
 <template>
   <div class="container" @click="click()">
     <img
-      class="icon"
-      align="left"
-      src="../assets/backIcon.png"
-      @click.stop="back()"
+        class="icon"
+        align="left"
+        src="../assets/backIcon.png"
+        @click.stop="back()"
+        @load="imgLoadedMethod"
     />
-    <img class="bottom" src="../assets/touch.png" />
+    <img class="bottom" src="../assets/touch.png"/>
+
+    <div class="ev6_con">
+      <div class="bubble-con"><img
+
+          :class="{ title: true, active: loaded}"
+          src="../assets/ev6_pic.png"
+      /><img
+          :class="{ title: true, active: loaded}"
+          src="../assets/ev6_title.png"
+      /></div>
+      <div class="bubble-con"><img
+          :class="{ test1: true, active: loaded }"
+          src="../assets/ev6_con1.png"
+          style="width: 85%"
+      /></div>
+      <div class="bubble-con"><img
+          :class="{ test2: true, active: loaded }"
+          src="../assets/ev6_con2.png"
+          style="width: 85%"
+      /></div>
+      <div class="bubble-con"><img
+          :class="{ test3: true, active: loaded }"
+          src="../assets/ev6_con3.png"
+          style="width: 85%"
+      /></div>
+      <div class="bubble-con"><img
+          :class="{ test4: true, active: loaded }"
+          src="../assets/ev6_con4.png"
+          style="width: 85%"
+      /></div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "Event6",
+  data: function () {
+    return {
+      loaded: false,
+    };
+  },
   methods: {
+    imgLoadedMethod() {
+      this.loaded = true;
+      console.log("on load", this.loaded);
+    },
     click() {
-      this.$router.push("Event7");
+      //this.$router.push("Event7");
     },
     back() {
       this.$router.go(-1);
@@ -25,10 +66,35 @@ export default {
 </script>
 
 <style scoped>
+.title {
+  opacity: 0;
+  transition: opacity 2s 1s;
+}
+
+.test1 {
+  opacity: 0;
+  transition: opacity 2s 3s;
+}
+
+.test2 {
+  opacity: 0;
+  transition: opacity 2s 5s;
+}
+
+.test3 {
+  opacity: 0;
+  transition: opacity 2s 7s;
+}
+
+.test4 {
+  opacity: 0;
+  transition: opacity 2s 9s;
+}
+
 .container {
   max-width: 500px;
   min-height: 100vh;
-  background-image: url("../assets/ev6.png");
+  background-image: url("../assets/ev6_bg.png");
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
@@ -38,6 +104,8 @@ export default {
 .container .icon {
   height: 35px;
   width: 35px;
+  position: absolute;
+  left: 0;
 }
 
 .container .bottom {
@@ -46,5 +114,39 @@ export default {
   bottom: 20px;
   left: 50%;
   transform: translate(-50%, 0);
+}
+
+img.active {
+  opacity: 1;
+}
+
+.ev6_con {
+  display: flex;
+  flex-direction: column;
+  padding-left: 30px;
+  padding-right: 30px;
+}
+.bubble-con:nth-child(1) {
+  padding-top: 20%;
+  padding-bottom: 10%;
+}
+.bubble-con {
+  padding-bottom: 5%;
+  display: flex;
+  height: 33px;
+  width: 100%;
+}
+.bubble-con img {
+  object-fit: contain;
+}
+.bubble-con:last-child {
+  height: 63px;
+}
+.bubble-con:nth-child(1) {
+  justify-content: center;
+  overflow: visible;
+}
+.bubble-con:nth-child(2n+3) {
+  justify-content: flex-end;
 }
 </style>
